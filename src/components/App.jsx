a06a0c789c496d27400efb7fb5b032a2b327e464
espadv69 <aitorjob69@gmail.com>
2024-08-06 10:34:34 +0200

    Update: - How to get data by form in native js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,32 @@
 import './App.css'
+import { useState } from 'react'
 import { MoviesRender } from './RenderMovies.jsx'
 import { useMovies } from './hooks/useMovies.js'
 
 function App() {
   // Get the mappedMovies data from the custom hook useMovies
-  const { movies: mappedMovies } = useMovies()
+  const { movies } = useMovies()
+  const [query, setQuery] = useState('')
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const { query } = Object.fromEntries(
+      new window.FormData(event.target)
+    )
+    console.log({ query })
+  }
+
+  const handleChange = (event) => {
+    setQuery(event.target.value)
+  }
 
   return (
     <div>
       <header>
-        <form className='form'>
+        <form className='form' onSubmit={handleSubmit}>
           <label>
             MOVIE SEARCHER.
-            <input placeholder='Avengers, Star Wars, The matrix...' />
+            <input onChange={handleChange} value={query} name='query' placeholder='Avengers, Star Wars, The matrix...' />
           </label>
           
           <button type='submit'>Search</button>
@@ -21,7 +35,7 @@ function App() {
 
       <main>
         {/* Render the MoviesRender component, passing the mappedMovies as a prop */}
-        <MoviesRender movies={mappedMovies}/>
+        <MoviesRender movies={movies}/>
       </main>
     </div>
   )
