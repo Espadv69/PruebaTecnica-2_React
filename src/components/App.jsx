f3aa6f52f4653ba88b1229aae5b40374918e23de
espadv69 <aitorjob69@gmail.com>
2024-08-06 10:03:11 +0200

    Update: - messages to better experience of user

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,9 +2,8 @@ import './App.css'
 import { MoviesRender } from './RenderMovies.jsx'
 import { useMovies } from './hooks/useMovies.js'
 
-
-
 function App() {
+  // Get the mappedMovies data from the custom hook useMovies
   const { movies: mappedMovies } = useMovies()
 
   return (
@@ -12,15 +11,16 @@ function App() {
       <header>
         <form className='form'>
           <label>
-            BUSCADOR DE PELÍCULAS.
+            MOVIE SEARCHER.
             <input placeholder='Avengers, Star Wars, The matrix...' />
           </label>
-
-          <button type='submit'>Buscar</button>
+          
+          <button type='submit'>Search</button>
         </form>
       </header>
 
       <main>
+        {/* Render the MoviesRender component, passing the mappedMovies as a prop */}
         <MoviesRender movies={mappedMovies}/>
       </main>
     </div>
