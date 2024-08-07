88f060dc134befe4b64fca1f466a44dfe10273de
espadv69 <aitorjob69@gmail.com>
2024-08-07 12:13:31 +0200

    Update: - Optimization.

--- src/components/hooks/useMovies.js
+++ src/components/hooks/useMovies.js
@@ -7,27 +7,33 @@ export function useMovies({ search, sort }) {
   const [error, setError] = useState(null)
   const previousSearch = useRef(search)
 
-  const getMovies = async () => {
-    if (search === previousSearch.current) return
+  const getMovies = useMemo(() => {
+    return async ({ search }) => {
+      if (search === previousSearch.current) return
 
-    try {
-      setLoading(true)
-      previousSearch.current = search
-      const newMovies = await searchMovies({ search })
-      setMovies(newMovies)
-    } catch (e) {
-      setError(e.message)
-    } finally {
-      setLoading(false)
+      try {
+        setLoading(true)
+        previousSearch.current = search
+        const newMovies = await searchMovies({ search })
+        setMovies(newMovies)
+      } catch (e) {
+        setError(e.message)
+      } finally {
+        setLoading(false)
+      }
+      
+      console.log('getMovies')
     }
-  }
+  }, []) 
 
   const sortedMovies = useMemo(() => {
-    console.log('MemoSortedMovies')
+    console.log('sortedMovie')
     return sort
       ? [...movies].sort((a, b) => a.title.localeCompare(b.title))
       : movies
   }, [sort, movies])
+
+
 
   // Return the mapped movies array
   return { movies: sortedMovies, getMovies, loading, error }
